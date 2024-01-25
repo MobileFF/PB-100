6e664da98b4d9fe986162f50f78848c246aa81af
MobileFF <mobile54@gmail.com>
2024-01-25 21:06:44 +0900

    CasUtil_PBSIM_Henkanを追加

---
+++ CasUtil_PBSIM_Henkan/casutil_pbsim_henkan.js
@@ -0,0 +1,124 @@
+const escape = [
+    ["\\\\","\\BS","＼"],
+    ["\\E-","\\EM","ｪ"],
+    ["\\E+","\\EX","ｴ"],
+    ["\\@","\\CI","○"],
+    ["\\SM","\\SG","∑"],
+    ["\\^","\\TR","△"],
+    ["\\*","\\CR","×"],
+    ["\\:","\\DV","÷"],
+    ["{","\\LA","←"],
+    ["|","\\DA","↓"],
+    ["}","\\RA","→"],
+    ["\\YN","\\\\","￥"],
+    ["\\.","\\DT","・"],
+    ["\\#","\\BX","■"],
+    //["^","\\UA","↑"],
+    // for PBPrinter
+    ["\\>=","\\GE","≧"],
+    ["\\<=","\\LE","≦"],
+    ["\\<>","\\NE","≠"],
+    [">=",">=","≧"],
+    ["<=","<=","≦"],
+    ["<>","<>","≠"],
+    ["\\PI","\\PI","π"],
+    ["\\DG","\\DG","°"],
+    ["\\SP","\\SP","♠"],
+    ["\\HT","\\HT","♥"],
+    ["\\DI","\\DI","♦"],
+    ["\\CL","\\CL","♣"],
+    ["\\MU","\\MU","μ"],
+    ["\\OM","\\OM","Ω"],
+    ["\\SQ","\\SQ","□"],
+    // CASutil : "THEN$" → PB-Sim : "THEN $"
+    ["THEN$","THEN $","THEN $"],
+]
+const escapeInString = [
+    ["^","\\UA","↑"],
+]
+
+document.getElementById("exe").addEventListener("click",event => {
+    const mode = document.getElementById("to_pbsim")
+    let from = 0
+    let to = 1
+    let prt = 2
+    if (mode.checked) {
+        console.log("mode:casutil to pbsim")
+    } else {
+        from = 1
+        to = 0
+        console.log("mode:pbsim to casutil")
+    }
+    const source_from = document.getElementById("source_from").value
+    // console.log(source_from)
+    let source_to = new String(source_from)
+    let source_prt = new String(source_from)
+    for(const e of escape) {
+        let regexp = new RegExp(
+            e[from]
+                .replace(/\\/gi,"\\\\")
+                .replace(/\./gi,"\\.")
+                .replace(/\^/gi,"\\^")
+                .replace(/\|/gi,"\\|")
+                .replace(/\$/gi,"\\$")
+                .replace(/\+/gi,"\\+")
+                .replace(/\*/gi,"\\*"),"gi")
+                
+        console.log(regexp)
+        source_to = source_to.replace(regexp,e[to])
+        source_prt = source_prt.replace(regexp,e[prt])
+        //console.log(source_to)
+    }
+
+    let pos1 = 0
+    let pos2 = 0
+    let quote = false
+    let literal = ""
+    for(let i=0;i<source_to.length;i++) {
+        if(source_to.charAt(i)=='"') {
+            quote = !quote
+            if (quote) {
+                pos1 = i
+                continue
+            } else {
+                console.log(literal)
+                let before_length = literal.length
+                pos2 = i
+                for(const e of escapeInString) {
+                    let regexp = new RegExp(
+                        e[from]
+                            .replace(/\\/gi,"\\\\")
+                            .replace(/\./gi,"\\.")
+                            .replace(/\^/gi,"\\^")
+                            .replace(/\|/gi,"\\|")
+                            .replace(/\$/gi,"\\$")
+                            .replace(/\+/gi,"\\+")
+                            .replace(/\*/gi,"\\*"),"gi")
+                    literal = literal.replace(regexp,e[to])
+                }
+                console.log(">>"+literal)
+                let after_length = literal.length
+                let offset = after_length - before_length
+                console.log("offset:"+offset)
+                source_to = source_to.substring(0,pos1+1)+literal+source_to.substring(pos2)
+                literal = ""
+                i = i + offset
+                // console.log(source_to)
+            }
+        }
+        if (quote) {
+            literal += source_to.charAt(i)
+        }
+    }
+
+    console.log(source_prt)
+    document.getElementById("source_to").textContent = source_to
+    document.getElementById("source_to").disabled = false
+    document.getElementById("printer_image").innerHTML = source_prt
+})
+
+document.getElementById("download").addEventListener("click",event => {
+    let content = document.getElementById("source_to").textContent 
+    let blob = new Blob([ content ], { "type" : "text/plain" })
+    document.getElementById("download").href = window.URL.createObjectURL(blob);
+})
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PB-100
+
+PB-100用のサンプルプログラムや支援ユーティリティです。
+一部のものは、弊ブログ([https://mobileff.blogspot.com/]([https://mobileff.blogspot.com/))にも掲載されています。
+
+## CasUtil_PBSIM_Henkan
+
+CASutil形式で作成したソースコードと、PB-Sim/Pokecom GO形式のソースコードの相互変換（特殊文字のエスケープシーケンスの置き換え）を行います。
